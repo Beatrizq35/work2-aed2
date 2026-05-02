0ee5e370db2f2184ebfb080b77b16c67d668355c
Jonathas-MT <jonathastrindade18@gmail.com>
2026-05-01 21:11:03 -0400

    arvorebinaria.c
    Ainda falta a função que ver a altura da árvore, o restante creio que esteja certo.

---
+++ arvorebinaria.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvorebinaria.h"
+
+No* criarNo(int valor) {
+    No* novo = (No*) malloc(sizeof(No));
+    novo->valor = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+No* inserir(No* raiz, int valor) {
+    if (raiz == NULL){
+        return criarNo(valor);
+    }
+    if (valor < raiz->valor)
+        raiz->esq = inserir(raiz->esq, valor);
+    else if (valor > raiz->valor)
+        raiz->dir = inserir(raiz->dir, valor);
+
+    return raiz;
+}
+
+
+No* buscar(No* raiz, int valor) {
+    if (raiz == NULL || raiz->valor == valor)
+        return raiz;
+
+    if (valor < raiz->valor)
+        return buscar(raiz->esq, valor);
+    else
+        return buscar(raiz->dir, valor);
+}
+
+void emOrdem(No* raiz) {
+    if (raiz != NULL) {
+        emOrdem(raiz->esq);
+        printf("%d ", raiz->valor);
+        emOrdem(raiz->dir);
+    }
+}
+
+void preOrdem(No* raiz) {
+    if (raiz != NULL) {
+        printf("%d ", raiz->valor);
+        preOrdem(raiz->esq);
+        preOrdem(raiz->dir);
+    }
+}
+
+void posOrdem(No* raiz) {
+    if (raiz != NULL) {
+        posOrdem(raiz->esq);
+        posOrdem(raiz->dir);
+        printf("%d ", raiz->valor);
+    }
+}
